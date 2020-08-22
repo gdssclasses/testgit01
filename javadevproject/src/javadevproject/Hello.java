468d9a96efe95cf81582bac5870dd5e0c3f6c429
HP <HP@DESKTOP-DAN7M6B>
2020-08-22 17:50:42 +0530

    new master hello java

---
+++ javadevproject/src/javadevproject/Hello.java
@@ -0,0 +1,15 @@
+package javadevproject;
+
+public class Hello {
+
+	public void pupAge() {
+		      int age = 0;
+		      age = age + 7;
+		      System.out.println("Puppy age is : " + age);
+		   }
+
+	public static void main(String args[]) {
+	   Hello test = new Hello();
+	   test.pupAge();
+		   }
+}
